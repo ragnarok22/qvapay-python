9a969456c0747642238f1751d96efe78b95b1dcd
Carlos Lugones <contact@lugodev.com>
2021-08-28 02:02:36 -0400

    perf: preparing for publishing to pypi

--- qvapay/__init__.py
+++ qvapay/__init__.py
@@ -4,12 +4,17 @@
  | | | \ \ / / _` | |_) / _` | | | |
  | |_| |\ V / (_| |  __/ (_| | |_| |
   \__\_\ \_/ \__,_|_|   \__,_|\__, |
-                              |___/ 
+                              |___/ ✔️✔️
 
+   Python SDK for the QvaPay API
 """
+
 from __future__ import absolute_import
-
-api_url = 'https://qvapay.com/api/v'
-
 from qvapay.resources import *
 from qvapay.client import Client
+
+__version__ = '0.0.1'
+__author__ = 'Carlos Lugones <contact@lugodev.com>'
+__all__ = []
+
+api_url = 'https://qvapay.com/api/v'
