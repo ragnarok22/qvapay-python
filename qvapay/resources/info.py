9e726800a779aa91de462893b382bf5ae849c814
Ozkar L. Garcell <ozkar.garcell@gmail.com>
2021-08-29 17:18:35 -0400

    feat: fixed info class and post init function

--- qvapay/resources/info.py
+++ qvapay/resources/info.py
@@ -1,24 +1,35 @@
 from dataclasses import dataclass
+from datetime import datetime
+from uuid import UUID
 
 
 @dataclass
-class Info(object):
+class Info:
     """
     QvaPay app info
     """
 
-    id: str = None
-    user_id: int = None
-    name: str = None
-    url: str = None
-    description: str = None
-    callback: str = None
-    logo: str = None
-    active: bool = False
-    enabled: bool = False
+    uuid: UUID
+    user_id: int
+    name: str
+    url: str
+    desc: str
+    callback: str
+    success_url: str
+    cancel_url: str
+    logo: str
+    active: bool
+    enabled: bool
+    card: int
+    created_at: datetime
+    updated_at: datetime
 
     def __post_init__(self):
         if self.active == 1:
             self.active = True
+        else:
+            self.active = False
         if self.enabled == 1:
             self.enabled = True
+        else:
+            self.enabled = False
