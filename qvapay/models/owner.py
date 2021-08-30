1e582fc525212c50d19abce90356840ac528fa98
Ozkar L. Garcell <ozkar.garcell@gmail.com>
2021-08-30 11:07:47 -0400

    feat: added alias to uuid field

--- qvapay/models/owner.py
+++ qvapay/models/owner.py
@@ -1,10 +1,13 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from uuid import UUID
 
+from dataclasses_json import config, dataclass_json
 
+
+@dataclass_json
 @dataclass
 class Owner:
-    id: UUID
+    id: UUID = field(metadata=config(field_name="uuid"))
     username: str
     name: str
     lastname: str
