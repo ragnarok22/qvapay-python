207e79b3bee7b1bae2fdb32711d26c6ed67f5d93
Carlos Lugones <contact@carloslugones.com>
2021-09-05 04:36:52 -0400

    chore: setting up next version

--- qvapay/v1/__init__.py
+++ qvapay/v1/__init__.py
@@ -11,6 +11,6 @@ from .models.paid_by import PaidBy  # noqa: F401
 from .models.transaction_detail import TransactionDetail  # noqa: F401
 from .models.transaction import Transaction  # noqa: F401
 
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 __author__ = "Carlos Lugones <contact@lugodev.com>"
 __all__ = []
