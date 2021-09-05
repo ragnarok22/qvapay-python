cb8ea76e9f37edf22e90a7d8f3695245c51df4e7
leynier <leynier41@gmail.com>
2021-09-05 09:10:57 -0400

    feat: add not required status message to QvaPayError

--- qvapay/v1/errors.py
+++ qvapay/v1/errors.py
@@ -1,6 +1,8 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from typing import Optional
 
 
 @dataclass
 class QvaPayError(Exception):
     status_code: int
+    status_message: Optional[str] = field(default=None)
