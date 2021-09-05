bc4106bb5e92755d7cc78b302589ac62c791eea2
leynier <leynier41@gmail.com>
2021-09-05 09:11:27 -0400

    feat: add post init validation to QvaPayAuth

--- qvapay/v1/auth.py
+++ qvapay/v1/auth.py
@@ -3,6 +3,8 @@ from os import environ
 
 from dotenv import load_dotenv
 
+from .errors import QvaPayError
+
 load_dotenv()
 
 
@@ -10,3 +12,11 @@ load_dotenv()
 class QvaPayAuth:
     qvapay_app_id: str = field(default=environ["QVAPAY_APP_ID"])
     qvapay_app_secret: str = field(default=environ["QVAPAY_APP_SECRET"])
+
+    def __post_init__(self):
+        if not self.qvapay_app_id and not self.qvapay_app_secret:
+            raise QvaPayError(0, "QVAPAY_APP_ID and QVAPAY_APP_SECRET are not setted")
+        elif not self.qvapay_app_id:
+            raise QvaPayError(0, "QVAPAY_APP_ID is not setted")
+        elif not self.qvapay_app_secret:
+            raise QvaPayError(0, "QVAPAY_APP_SECRET is not setted")
