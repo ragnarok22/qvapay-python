a280ecaced950e65b02d8dd2f1a56ef57ee0fd3d
leynier <leynier41@gmail.com>
2021-10-20 22:25:23 -0400

    test: use pytest fixtures

--- tests/v1/_async/test_client.py
+++ tests/v1/_async/test_client.py
@@ -2,11 +2,19 @@ from random import random
 from uuid import uuid4
 
 from httpx import Timeout
+from pytest import fixture
 from pytest import mark as pytest_mark
 
 from qvapay.v1 import AsyncQvaPayClient, QvaPayAuth, QvaPayError
 
 TIMEOUT = 20
+
+
+@fixture(name="client")
+async def create_client():
+    client = AsyncQvaPayClient.from_auth(QvaPayAuth(), timeout=Timeout(TIMEOUT))
+    yield client
+    await client.close()
 
 
 @pytest_mark.anyio
@@ -20,29 +28,23 @@ async def test_error():
 
 
 @pytest_mark.anyio
-async def test_get_info():
-    client = AsyncQvaPayClient.from_auth(QvaPayAuth(), timeout=Timeout(TIMEOUT))
+async def test_get_info(client: AsyncQvaPayClient):
     await client.get_info()
 
 
 @pytest_mark.anyio
-async def test_get_balance():
-    client = AsyncQvaPayClient.from_auth(QvaPayAuth(), timeout=Timeout(TIMEOUT))
+async def test_get_balance(client: AsyncQvaPayClient):
     await client.get_balance()
 
 
 @pytest_mark.anyio
-async def test_create_invoice():
-    client = AsyncQvaPayClient.from_auth(QvaPayAuth(), timeout=Timeout(TIMEOUT))
+async def test_create_invoice(client: AsyncQvaPayClient):
     await client.create_invoice(random(), "Invoice for testing", str(uuid4()))
 
 
 @pytest_mark.anyio
-async def test_get_transactions():
-    async with AsyncQvaPayClient.from_auth(
-        QvaPayAuth(), timeout=Timeout(TIMEOUT)
-    ) as client:
-        result = await client.get_transactions()
-        if result.data:
-            item = result.data[0]
-            await client.get_transaction(item.id)
+async def test_get_transactions(client: AsyncQvaPayClient):
+    result = await client.get_transactions()
+    if result.data:
+        item = result.data[0]
+        await client.get_transaction(item.id)
--- tests/v1/_sync/test_client.py
+++ tests/v1/_sync/test_client.py
@@ -2,11 +2,19 @@ from random import random
 from uuid import uuid4
 
 from httpx import Timeout
+from pytest import fixture
 from pytest import mark as pytest_mark
 
 from qvapay.v1 import QvaPayAuth, QvaPayError, SyncQvaPayClient
 
 TIMEOUT = 20
+
+
+@fixture(name="client")
+def create_client():
+    client = SyncQvaPayClient.from_auth(QvaPayAuth(), timeout=Timeout(TIMEOUT))
+    yield client
+    client.close()
 
 
 @pytest_mark.anyio
@@ -20,27 +28,23 @@ def test_error():
 
 
 @pytest_mark.anyio
-def test_get_info():
-    client = SyncQvaPayClient.from_auth(QvaPayAuth(), timeout=Timeout(TIMEOUT))
+def test_get_info(client: SyncQvaPayClient):
     client.get_info()
 
 
 @pytest_mark.anyio
-def test_get_balance():
-    client = SyncQvaPayClient.from_auth(QvaPayAuth(), timeout=Timeout(TIMEOUT))
+def test_get_balance(client: SyncQvaPayClient):
     client.get_balance()
 
 
 @pytest_mark.anyio
-def test_create_invoice():
-    client = SyncQvaPayClient.from_auth(QvaPayAuth(), timeout=Timeout(TIMEOUT))
+def test_create_invoice(client: SyncQvaPayClient):
     client.create_invoice(random(), "Invoice for testing", str(uuid4()))
 
 
 @pytest_mark.anyio
-def test_get_transactions():
-    with SyncQvaPayClient.from_auth(QvaPayAuth(), timeout=Timeout(TIMEOUT)) as client:
-        result = client.get_transactions()
-        if result.data:
-            item = result.data[0]
-            client.get_transaction(item.id)
+def test_get_transactions(client: SyncQvaPayClient):
+    result = client.get_transactions()
+    if result.data:
+        item = result.data[0]
+        client.get_transaction(item.id)
